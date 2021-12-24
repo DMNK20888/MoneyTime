5a3d99fe0b5ce662903b24cff48457dbef7afd40
DMNK20888 <dominikpbarlow@gmail.com>
2021-12-24 12:04:24 -0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, "../client/public")));
 
 //All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '.../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 
